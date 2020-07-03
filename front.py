ff1caf29fe21089f36fe9603d568b7f9b6d76089
Lada Dudnikova <ladud111@gmail.com>
2020-07-03 12:26:39 +0300

    captcha

--- front.py
+++ front.py
@@ -2,8 +2,8 @@ import telebot
 import logging
 #from pprint import pprint
 
-bot = telebot.TeleBot("Your ACESS_TOKEN")
-group_id = "here goes the group ID where the forms go"
+bot = telebot.TeleBot("1389122885:AAHFnzypvbn4rYDtFBaBZ-syXddhSBijmnM")
+group_id = -447685531
 
 user_data = {}
 
@@ -12,6 +12,7 @@ class User:
         self.first_name = first_name
         self.course = ''
         self.username = user_name
+        self.passed_captcha = False
 
 logger = telebot.logger  # set logger
 telebot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
@@ -31,7 +32,7 @@ def send_welcome(message):
         msg = bot.send_message(message.chat.id, "Напишите свои имя и фамилию")
         bot.register_next_step_handler(msg, process_firstname_step)
     else:
-        bot.send_message(message.chat.id, "Вы уже зарегистрировались, ждите письма через неделю")
+        bot.send_message(message.chat.id, "Вы уже регистрировались, ждите письма через неделю, или напишите Ладе, если сомневаетесь в успехе.")
 
 
 # markups to keyboard
@@ -51,14 +52,44 @@ def process_firstname_step(message):
     msg = bot.send_message(message.chat.id, "Вы первый-второй курс или старше?", reply_markup=keyboard_course)
     bot.register_next_step_handler(msg, process_course_step)
 
-
-
+import random
 
 def process_course_step(message):
     user_id = message.from_user.id
     user = user_data[user_id]
     user.course = message.text
-    bot.send_message(message.chat.id, "Вы успешно зарегистрированы! Ждите ответа через неделю" , reply_markup=markupHide)
-    bot.send_message(group_id, user.first_name+ " @" + user.username + " зарегался. Это" + user.course, parse_mode="Markdown")
+    bot.send_message(message.chat.id, "Вопрос-капча: прошу ответить русскими буквами, начиная с большой буквы, тестируется хэш от строки", reply_markup=markupHide)
+    the_question = random.choice(list(question_dict.keys()))
+    msg = bot.send_message(message.chat.id, the_question)
+    bot.register_next_step_handler(msg, lambda m: registration_fail_success(m, the_question))
+
+def bare_captcha(message):
+    the_question = random.choice(list(question_dict.keys()))
+    msg = bot.send_message(message.chat.id, the_question)
+    bot.register_next_step_handler(msg, lambda m: registration_fail_success(m, the_question))
+
+import base64
+
+def registration_fail_success(message, the_question):
+    user_id = message.from_user.id
+    user = user_data[user_id]
+    text = message.text
+    text_binary = text.encode(encoding='utf_8')
+
+    if base64.b64encode(text_binary) == question_dict[the_question]:
+        bot.send_message(group_id, user.first_name+ " @" + user.username + " зарегался. Это" + user.course, parse_mode="Markdown")
+        bot.send_message(message.chat.id, "Вы успешно зарегистрированы! Ждите ответа через неделю" )
+    else:
+         msg = bot.send_message(message.chat.id, "Ответ не принят, ещё один случайный вопрос: ")
+         bot.send_message(group_id, user.first_name+ " @" + user.username + " не ответил на капчу. Это" + user.course, parse_mode="Markdown")
+         bot.register_next_step_handler(msg, bare_captcha)
+
+question_dict = { "Имя первого декана матфака: " : b'0KHQtdGA0LPQtdC5',
+                    "Имя декана матфака с 2015 года:" : b'0JLQu9Cw0LTQu9C10L0=',
+                    "Имя декана матфака с 2020 года:" : b'0KHQsNCx0LjRgA=='
+                    }
+
 
 bot.polling()
+
+
