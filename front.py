9ba7793b7c328f4e19406f55487c4a8cc56a8c51
LadaEven <ladud111@gmail.com>
2020-07-06 09:37:31 +0300

    Update front.py
    rewrote logic as conditions, going to set a web_app to make a web_hook

--- front.py
+++ front.py
@@ -2,99 +2,107 @@ import telebot
 import logging
 #from pprint import pprint
 
-bot = telebot.TeleBot("")
-group_id = ...
+bot = telebot.TeleBot("1076370701:AAEe-ySnW9VtQgXjK04FaUE15ZxXI1xXn6Y")
+group_id = -447685531
 
 user_data = {}
 
 #import pickle
 
-
-
-
 class User:
-    def __init__(self, first_name, user_name):
-        self.first_name = first_name
+    def __init__(self, user_name):
+        self.first_name = ''
         self.course = ''
         self.username = user_name
-        self.passed_captcha = False
+        self.question = ''
+        self.condition = 0
+        #conditions 1 -- entered correct name
+        # 2 -- entered correct course
+        # 3 -- passed_captcha
+
+condition_dict = {
+                       0 : " should enter name",
+                        1 : "entered correct name, should enter course",
+                       2 : "entered correct course, should answer the question",
+                       3 : "passed captcha, wait for answer" }
 
 logger = telebot.logger  # set logger
 telebot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
 
+@bot.message_handler(commands=['start'])
+def welcome_message(message):
+    if(message.chat.id not in user_data):
+        bot.send_message(group_id, "начал работу")
+        bot.send_message(message.chat.id, "Напишите свои имя и фамилию. Обрабатываются только текстовые сообщения")
+        user_data[message.chat.id] = User(message.from_user.username)
+    else:
+        bot.send_message(message.chat.id, "вы уже начали регистрацию, вы остановились на шаге " + condition_dict[user_data[message.chat.id].condition])
 
 @bot.message_handler(commands=['help', 'about'])
-def helper(message):
+def help_about(message):
+    bot.send_message(message.chat.id ,"Вопросы, преложения, сообщить, что бот упал: пишите Ладе @OrangeLine. Код на github: LadaEven/mf_HSE_match_front_bot")
     if(message.chat.id not in user_data):
         bot.send_message(message.chat.id, "Я бот, который зарегистрирует вас, а потом вам придёт два ника в телеграме, кто сделал также.")
     else:
-        bot.send_message(message.chat.id, "Вы уже зарегистрировались, ждите письма через неделю. Если что-то пошло не так, пишите Ладе @OrangeLine")
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    if(message.chat.id not in user_data):
-        msg = bot.send_message(message.chat.id, "Напишите свои имя и фамилию")
-        bot.register_next_step_handler(msg, process_firstname_step)
-    else:
-        bot.send_message(message.chat.id, "Вы уже регистрировались, ждите письма через неделю, или напишите Ладе, если сомневаетесь в успехе.")
+        bot.send_message(message.chat.id, "вы уже начали регистрацию, вы остановились на шаге " + condition_dict[user_data[message.chat.id].condition])
 
 
 # markups to keyboard
 keyboard_course = telebot.types.ReplyKeyboardMarkup()
 keyboard_course.row('1-2 курс', '3-4 курс, магистр, аспирант')
-# markup to force reply
-force_reply = telebot.types.ForceReply(selective=False)
 # markup to hide previous markup
 markupHide = telebot.types.ReplyKeyboardRemove(selective=False)
 
-
-
-
-def process_firstname_step(message):
-    user_id = message.from_user.id
-    user_data[user_id] = User(message.text, message.from_user.username)
-    msg = bot.send_message(message.chat.id, "Вы первый-второй курс или старше?", reply_markup=keyboard_course)
-    bot.register_next_step_handler(msg, process_course_step)
-
 import random
 
-def process_course_step(message):
-    user_id = message.from_user.id
-    user = user_data[user_id]
-    user.course = message.text
-    bot.send_message(message.chat.id, "Вопрос-капча: прошу ответить русскими буквами, начиная с большой буквы, тестируется хэш от строки", reply_markup=markupHide)
-    the_question = random.choice(list(question_dict.keys()))
-    msg = bot.send_message(message.chat.id, the_question)
-    bot.register_next_step_handler(msg, lambda m: registration_fail_success(m, the_question))
-
-def bare_captcha(message):
-    the_question = random.choice(list(question_dict.keys()))
-    msg = bot.send_message(message.chat.id, the_question)
-    bot.register_next_step_handler(msg, lambda m: registration_fail_success(m, the_question))
+@bot.message_handler(content_types=['text'])
+def change_condition(message):
+    if(message.chat.id in user_data):
+        user = user_data[message.chat.id]
+        condition = user.condition
+        if condition == 0:
+            if len(message.text.split()) == 2:
+                bot.send_message(message.chat.id, "Записал имя и фамилию")
+                user.first_name = message.text
+                user.condition = 1
+                bot.send_message(message.chat.id, "Вы первый-второй курс или старше?", reply_markup=keyboard_course)
+            else:
+                bot.send_message(message.chat.id, "Вы ввели не два слова")
+        elif condition == 1:
+            if message.text in ["1-2 курс", "3-4 курс, магистр, аспирант"]:
+                bot.send_message(message.chat.id, "Записал курс", reply_markup=markupHide)
+                user.course = message.text
+                user.condition = 2
+                bot.send_message(message.chat.id, "Вопрос-капча: прошу ответить русскими буквами, начиная с большой буквы, тестируется хэш от строки")
+                the_question = random.choice(list(question_dict.keys()))
+                bot.send_message(message.chat.id, the_question)
+                user.question = the_question
+            else:
+                bot.send_message(message.chat.id, "пожалуйста, воспользуйтесь встроенной клавиатурой")
+        elif condition == 2:
+            if captcha(message, user.question):
+                user.condition = 3
+                bot.send_message(group_id, user.first_name+ " @" + user.username + " зарегался. Это" + user.course)
+                bot.send_message(message.chat.id, "Вы успешно зарегистрированы! Ждите ответа через неделю" )
+            else:
+                bot.send_message(message.chat.id, "вы не прошли капчу, попробуйте ещё раз")
+                bot.send_message(group_id, user.first_name+ " @" + user.username + " не ответил на капчу. Это" + user.course)
 
 import base64
 
-def registration_fail_success(message, the_question):
-    user_id = message.from_user.id
-    user = user_data[user_id]
-    text = message.text
-    text_binary = text.encode(encoding='utf_8')
+def captcha(message, the_question):
+    text_binary = message.text.encode(encoding='utf_8')
 
     if base64.b64encode(text_binary) == question_dict[the_question]:
-        bot.send_message(group_id, user.first_name+ " @" + user.username + " зарегался. Это" + user.course, parse_mode="Markdown")
-        bot.send_message(message.chat.id, "Вы успешно зарегистрированы! Ждите ответа через неделю" )
-        user.passed_captcha = True
+        return True
     else:
-         bot.send_message(message.chat.id, "Ответ не принят, ещё один случайный вопрос: ")
-         bot.send_message(group_id, user.first_name+ " @" + user.username + " не ответил на капчу. Это" + user.course, parse_mode="Markdown")
-         bare_captcha(message)
+        return False
+
 
 question_dict = { "Имя первого декана матфака: " : b'0KHQtdGA0LPQtdC5',
                     "Имя декана матфака с 2015 года:" : b'0JLQu9Cw0LTQu9C10L0=',
                     "Имя декана матфака с 2020 года:" : b'0KHQsNCx0LjRgA=='
                     }
 
-
-bot.polling()
-
+secret = ""
+bot.set_webhook("https://PlumpiePyTelegramBotAPI.pythonanywhere.com/{}".format(secret), max_connections=1)
