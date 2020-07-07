ac1412ee57019dd8de614238d99776f3df31fd38
LadaEven <ladud111@gmail.com>
2020-07-07 07:53:54 +0300

    Добавление поля внешних контактов.

--- front.py
+++ front.py
@@ -3,7 +3,7 @@ import logging
 #from pprint import pprint
 
 bot = telebot.TeleBot("")
-group_id = GROUP_NUMBER
+group_id = GROUP_ID
 
 
 
@@ -12,7 +12,7 @@ import dill
 
 
 try:
-    with open("database.txt", "rb") as f:
+    with open("database.bin", "rb") as f:
         user_data = dill.load(f)
 except EOFError:
     user_data = {}
@@ -22,12 +22,14 @@ except EOFError:
 #далее логика работы.
 
 class User:
-    def __init__(self, user_name):
+    def __init__(self, chat_id):
         self.first_name = ''
         self.course = ''
-        self.username = user_name
+        self.chat_id = chat_id
+        self.username = 'None'
         self.question = ''
         self.condition = 0
+        self.add_info = 'None'
         #conditions 1 -- entered correct name
         # 2 -- entered correct course
         # 3 -- passed_captcha
@@ -35,8 +37,9 @@ class User:
 condition_dict = {
                        0 : " should enter name",
                         1 : "entered correct name, should enter course",
-                       2 : "entered correct course, should answer the question",
-                       3 : "passed captcha, thank you!" }
+                        2: "entered course, should add contacts",
+                       3 : "entered contacts, should answer the question",
+                       4 : "passed captcha, thank you!" }
 
 logger = telebot.logger  # set logger
 telebot.logger.setLevel(logging.WARNING) # Outputs debug messages to console.
@@ -44,28 +47,41 @@ telebot.logger.setLevel(logging.WARNING) # Outputs debug messages to console.
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
     if(message.chat.id not in user_data):
-        user_data[message.chat.id] = User(message.from_user.username)
+        user = User(message.chat.id)
+        user_data[message.chat.id] = user
+        if ( message.from_user.username ):
+                user.username = message.from_user.username
         bot.send_message(message.chat.id, "Напишите свои имя и фамилию")
+        bot.send_message(group_id, str(message.chat.id)+ " " + user.username + " Начал регистрацию")
+
     else:
         bot.send_message(message.chat.id, "Вы уже начали регистрацию, вы остановились на шаге " + condition_dict[user_data[message.chat.id].condition])
 
-@bot.message_handler(commands=['help', 'about'])
-def help_about(message):
-    bot.send_message(message.chat.id ,"Вопросы, предложения, сообщить, что бот упал: пишите Ладе @OrangeLine." +
-    "Код на github: LadaEven/mf_HSE_match_front_bot" + " если хотите исправить данные, отправьте команду /reset")
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id ,"Вопросы, предложения, сообщить, что бот упал: пишите Ладе @OrangeLine.")
+
     if(message.chat.id not in user_data):
-        bot.send_message(message.chat.id, "Я бот, который зарегистрирует вас, а потом вам придёт два ника в телеграме, кто сделал также.")
+        bot.send_message("такие дела")
     else:
-        bot.send_message(message.chat.id, "Вы уже начали регистрацию, вы остановились на шаге " +
-        condition_dict[user_data[message.chat.id].condition])
+        bot.send_message(message.chat.id, "Вы уже начали регистрацию, вы остановились на шаге " + condition_dict[user_data[message.chat.id].condition]+
+         " если хотите исправить данные, отправьте команду /reset")
+
+@bot.message_handler(commands=['about'])
+def about(message):
+ bot.send_message(message.chat.id , "Код на github: LadaEven/mf_HSE_match_front_bot" +
+ " Этот бот познакомит старшекурсников и младшекурсников матфака. Вы отправляете заявку, заявка сохраняется в питоновском словаре," +
+ "и через неделю я мэтчу людей -- по два контакта, если их будет не слишком много - я сделаю это вручную." )
+
 
 @bot.message_handler(commands=['reset'])
 def reset_registration(message):
     if message.chat.id in user_data:
         user_data[message.chat.id].condition = 0
         bot.send_message(message.chat.id, "Окей, теперь можно перерегаться, напишите имя и фамилию")
+        bot.send_message(group_id, str(message.chat.id) + " Перерегистрируется")
     else:
-        bot.send_message(message.chat.id, "вы ещё не регались")
+        bot.send_message(message.chat.id, "Вы ещё не регались")
 
 # markups to keyboard
 keyboard_course = telebot.types.ReplyKeyboardMarkup()
@@ -92,24 +108,34 @@ def change_condition(message):
             if message.text in ["1-2 курс", "3-4 курс, магистр, аспирант"]:
                 bot.send_message(message.chat.id, "Записал курс", reply_markup=markupHide)
                 user.course = message.text
+                bot.send_message(message.chat.id, "При желании, укажите дополнительные контакты: почта, инстаграм, вк. ")
                 user.condition = 2
-                bot.send_message(message.chat.id, "Вопрос-капча: прошу ответить русскими буквами, начиная с большой буквы, тестируется хэш от строки")
-                the_question = random.choice(list(question_dict.keys()))
-                bot.send_message(message.chat.id, the_question)
-                user.question = the_question
+
             else:
-                bot.send_message(message.chat.id, "пожалуйста, воспользуйтесь встроенной клавиатурой")
+                bot.send_message(message.chat.id, "Пожалуйста, воспользуйтесь встроенной клавиатурой")
+
         elif condition == 2:
+            #contacts
+            user.add_info = message.text
+            bot.send_message(message.chat.id , "Записал доп.контакты")
+            user.condition = 3
+            bot.send_message(message.chat.id, "Вопрос-капча: прошу ответить русскими буквами, начиная с большой буквы, тестируется хэш от строки")
+            the_question = random.choice(list(question_dict.keys()))
+            bot.send_message(message.chat.id, the_question)
+            user.question = the_question
+
+
+        elif condition == 3:
+            msg_who = str(message.chat.id)+ " " + user.first_name + " @" + user.username+ " "  + user.add_info  + " " + user.course
             if captcha(message, user.question):
-                user.condition = 3
-                bot.send_message(group_id, user.first_name+ " @" + user.username + " зарегался. Это" + user.course)
-                bot.send_message(message.chat.id, str(message.chat.id) +  " Вы успешно зарегистрированы! Ждите ответа через неделю" )
-                with open("database.txt", "wb") as f:
+                user.condition = 4
+                bot.send_message(group_id, msg_who + " зарегался")
+                bot.send_message(message.chat.id,  " Вы успешно зарегистрированы! Ждите ответа через неделю" )
+                with open("database.bin", "wb") as f:
                     dill.dump(user_data, f)
             else:
-                bot.send_message(message.chat.id, "вы не прошли капчу, попробуйте ещё раз")
-                bot.send_message(group_id, str(message.chat.id)+ " " + user.first_name+ " @" + user.username + " не ответил на капчу. Это" + user.course)
-
+                bot.send_message(message.chat.id, "Вы не прошли капчу, попробуйте ещё раз")
+                bot.send_message(group_id, msg_who  + " не прошёл капчу")
 import base64
 
 def captcha(message, the_question):
